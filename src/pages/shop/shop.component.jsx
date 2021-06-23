763f1e9553571cb11ac349d35bc4eaf8ee224323
revanthkumaar <iamrevanthkumar@gmail.com>
2021-06-23 10:42:20 +0530

    sample

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,13 +1,9 @@
 import React from 'react';
-import CollectionPreview from '../../components/collection-preview/collection-preview';
 
 class ShopPage extends React.Component {
   constructor(props) {
     super(props);
-    /*
-    this.state = {
-      collections: SHOP_DATA //removing this internal state as we moved data to redux
-    };*/
+   
   }
 
   render() {
