6c13fe7c49d31b36c487f19c2e6fe5339bbf6d2a
revanthkumaar <iamrevanthkumar@gmail.com>
2021-06-25 11:41:44 +0530

    implement with login

--- src/redux/authentication/signin-homepage.js
+++ src/redux/authentication/signin-homepage.js
@@ -10,5 +10,35 @@ function HomePage() {
 
     useEffect(() => {
         dispatch(userActions.getAll())
-    })
+    },[]);
+
+    function handleDeleteUser(id) {
+        dispatch(userActions.delete(id));
+    }
+    return (
+         <div className="col-lg-8 offset-lg-2">
+            <h1>Hi {user.firstName}!</h1>
+            <p>You're logged in with React Hooks!!</p>
+            <h3>All registered users:</h3>
+            {users.loading && <em>Loading users...</em>}
+            {users.error && <span className="text-danger">ERROR: {users.error}</span>}
+            {users.items &&
+                <ul>
+                    {users.items.map((user, index) =>
+                        <li key={user.id}>
+                            {user.firstName + ' ' + user.lastName}
+                            {
+                                user.deleting ? <em> - Deleting...</em>
+                                : user.deleteError ? <span className="text-danger"> - ERROR: {user.deleteError}</span>
+                                : <span> - <a onClick={() => handleDeleteUser(user.id)} className="text-primary">Delete</a></span>
+                            }
+                        </li>
+                    )}
+                </ul>
+            }
+            <p>
+                <Link to="/login">Logout</Link>
+            </p>
+        </div>
+    )
 }
--- src/redux/authentication/userService.js
+++ src/redux/authentication/userService.js
@@ -1,11 +1,84 @@
-import {authHeader} from './auth-header'
+import config from 'config';
+import { authHeader } from './auth-header';
 
-function getAll{
+export const userService = {
+    login,
+    logout,
+    register,
+    getAll,
+    getById,
+    update,
+    delete: _delete
+};
+
+function login(username, password) {
     const requestOptions = {
-        method:'GET',
-        headers:authHeader()
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ username, password })
     };
-    return fetch(`${config.apiUrl}/users/${id}`, requestOptions).then(handleResponse)
+
+    return fetch(`${config.apiUrl}/users/authenticate`, requestOptions)
+        .then(handleResponse)
+        .then(user => {
+            // store user details and jwt token in local storage to keep user logged in between page refreshes
+            localStorage.setItem('user', JSON.stringify(user));
+
+            return user;
+        });
+}
+
+function logout() {
+    // remove user from local storage to log user out
+    localStorage.removeItem('user');
+}
+
+function getAll() {
+    const requestOptions = {
+        method: 'GET',
+        headers: authHeader()
+    };
+
+    return fetch(`${config.apiUrl}/users`, requestOptions).then(handleResponse);
+}
+
+function getById(id) {
+    const requestOptions = {
+        method: 'GET',
+        headers: authHeader()
+    };
+
+    return fetch(`${config.apiUrl}/users/${id}`, requestOptions).then(handleResponse);
+}
+
+function register(user) {
+    const requestOptions = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(user)
+    };
+
+    return fetch(`${config.apiUrl}/users/register`, requestOptions).then(handleResponse);
+}
+
+function update(user) {
+    const requestOptions = {
+        method: 'PUT',
+        headers: { ...authHeader(), 'Content-Type': 'application/json' },
+        body: JSON.stringify(user)
+    };
+
+    return fetch(`${config.apiUrl}/users/${user.id}`, requestOptions).then(handleResponse);;
+}
+
+// prefixed function name with underscore because delete is a reserved word in javascript
+function _delete(id) {
+    const requestOptions = {
+        method: 'DELETE',
+        headers: authHeader()
+    };
+
+    return fetch(`${config.apiUrl}/users/${id}`, requestOptions).then(handleResponse);
 }
 
 function handleResponse(response) {
@@ -21,6 +94,7 @@ function handleResponse(response) {
             const error = (data && data.message) || response.statusText;
             return Promise.reject(error);
         }
+
         return data;
     });
 }
